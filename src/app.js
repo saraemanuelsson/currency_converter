b93367dd740cca39211293f181d1ff310db0801a
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-23 16:35:39 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+    new Vue({
+        el: "#app",
+        data: {
+
+        },
+        computed: {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+
+        }
+    })
+})
