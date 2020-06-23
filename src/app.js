6d7925c5e9f57ef1633a3979365ff8960e603460
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-23 16:45:50 +0100

    Add populate rates method

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import Vue from "vue";
 
 document.addEventListener("DOMContentLoaded", () => {
+    
     new Vue({
         el: "#app",
         data: {
-
+            rates: [],
         },
         computed: {
 
         },
         mounted() {
-
+            this.populateRates();
         },
         methods: {
-
+            populateRates: function() {
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(result => result.json())
+                .then(rateData => this.rates(rateData));
+            }
         }
     })
 })
